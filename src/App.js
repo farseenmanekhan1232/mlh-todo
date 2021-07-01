380285faca07f2141a5c9849270857a42af55fcb
Mohammad Farseen <farseenmanekhan1232@gmail.com>
2021-07-01 16:01:53 +0530

    improvements

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import "./App.css"
 
 class App extends React.Component {
   state = {
-    tasks: []
+    tasks: [
+    "Guess the 2022 Season Mascot",
+    "Create a hack promoting equal access to CS education and host it on a .Tech domain!",
+    "Build a Search Hack using Jina AI",
+    "Build a Water Hack",
+    "Share your Chrome Dino Score",
+    "Create a Graphic about a Misconception in Technology",
+    "Build a Recipe Maker",
+    "Develop or Redesign your Portfolio Website",
+    "Use a Maps API",
+    "Create a Scheduler",
+    "Create a Financial Hack",
+    "Write a blog post about your plans for the 2022 Hackathon Season",
+    "Create a Random Number Generator",
+    "Build a Discord bot using Replit",
+    ]
   };
 
   handleSubmit = task => {
@@ -23,7 +38,7 @@ class App extends React.Component {
         <div className='card frame'>
           <Header numTodos={this.state.tasks.length} />
           <TodoList tasks={this.state.tasks} onDelete={this.handleDelete} />
-          <SubmitForm onFormSubmit={this.handleSubmit} />
+           {/*<SubmitForm onFormSubmit={this.handleSubmit} />*/}
         </div>
       </div>
     );
@@ -62,7 +77,7 @@ const Header = (props) => {
   return(
     <div className='card-header'>
       <h1 className='card-header-title header'>
-        You have {props.numTodos} Todos
+        You have {props.numTodos} Challenges to Try
       </h1>
     </div>
   )
@@ -84,7 +99,7 @@ const Todo = (props) => {
   return(
     <div className='list-item'>
       <div>{props.content}</div>
-      <div class="delete is-pulled-right" onClick={() => {props.onDelete(props.id)}}>del</div>
+      <div class="delete is-pulled-right" onClick={() => {props.onDelete(props.id)}}>Done</div>
     </div >
   );
 }
