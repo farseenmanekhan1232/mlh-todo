ac181836b975b21d2e366094ce726c9995de2727
Mohammad Farseen <farseenmanekhan1232@gmail.com>
2021-07-01 16:28:49 +0530

    improvements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from "react" ; 
-import "./App.css"
+import "./App.css";
+import  {Challenges} from "./challenges.js";
+let arr = Challenges;
 
 
 class App extends React.Component {
   state = {
-    tasks: [
-    "Guess the 2022 Season Mascot",
-    "Create a hack promoting equal access to CS education and host it on a .Tech domain!",
-    "Build a Search Hack using Jina AI",
-    "Build a Water Hack",
-    "Share your Chrome Dino Score",
-    "Create a Graphic about a Misconception in Technology",
-    "Build a Recipe Maker",
-    "Develop or Redesign your Portfolio Website",
-    "Use a Maps API",
-    "Create a Scheduler",
-    "Create a Financial Hack",
-    "Write a blog post about your plans for the 2022 Hackathon Season",
-    "Create a Random Number Generator",
-    "Build a Discord bot using Replit",
-    ]
+    tasks: arr
   };
 
   handleSubmit = task => {
---
+++ src/challenges.js
@@ -0,0 +1,19 @@
+export const Challenges = [
+"Guess the 2022 Season Mascot",
+"Create a hack promoting equal access to CS education and host it on a  Tech domain!",
+"Build a Search Hack using Jina AI",
+"Build a Water Hack",
+"Share your Chrome Dino Score",
+"Create a Graphic about a Misconception in Technology",
+"Build a Recipe Maker",
+"Develop or Redesign your Portfolio Website",
+"Use a Maps API",
+"Create a Scheduler",
+"Create a Financial Hack",
+"Write a blog post about your plans for the 2022 Hackathon Season",
+"Create a Random Number Generator",
+"Build a Discord bot using Replit",
+]
+
+
+
